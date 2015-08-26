3137414c2775a927c7361c7104d0f4890b2d1920
Ethan Seguin <ethan.seguin@techhigh.us>
2015-08-25 21:44:48 -0400

    Apples

---
+++ apples.java
@@ -0,0 +1,9 @@
+
+ class apples {
+public static void main (String args[]){
+	tuna tunaObject = new tuna("Tara Strong");
+	tuna tunaObject2 = new tuna ("Lauren Faust");
+	tunaObject.saying();
+	tunaObject2.saying();
+}
+}
